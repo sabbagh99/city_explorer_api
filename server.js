0d63610d9b9a15528a939c600feddd3abc431271
AHMAD ALSABBAGH <a.sameer1999@gmail.com>
2021-02-21 14:08:33 +0200

    bacsics

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 'use strict';
+
+let express = require('express');
+
+
+let app = express();
+
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT,()=>{
+    console.log('the app is listen on port'+PORT);
+})
