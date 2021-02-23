690d79534f78c22c7d691b60c80ca69de21379af
AHMAD ALSABBAGH <a.sameer1999@gmail.com>
2021-02-23 16:53:30 +0200

    add lab-08

--- server.js
+++ server.js
@@ -25,26 +25,26 @@ app.get('*', handleerror);
 //  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>handle location<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 function handleLocation(req, res) {
 
-  let searchQuery = req.query.city;
+    let searchQuery = req.query.city;
 
-  getLocationData(searchQuery).then(locationData => {
-    res.status(200).send(locationData);
-  });
+    getLocationData(searchQuery).then(locationData => {
+        res.status(200).send(locationData);
+    });
 
 }
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>handle weather<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 function handleWeather(req, res) {
-  getWeatherDeta(req, res).then(data => {
-    res.status(200).send(data);
-  });
+    getWeatherDeta(req, res).then(data => {
+        res.status(200).send(data);
+    });
 }
 
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>handle 404 function<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 function handleerror(req, res) {
-  res.status(404).send('Sorry, the page you are trying to access does not exist....');
+    res.status(404).send('Sorry, the page you are trying to access does not exist....');
 }
 
 // handle park
@@ -58,69 +58,69 @@ function handleerror(req, res) {
 //=============Get wether data===============
 
 function getWeatherDeta(req, res) {
-  console.log(req.query);
-  const query = {
+    console.log(req.query);
+    const query = {
 
-    key: process.env.WEATHER_API_KEY,
-    city: req.query.search_query
-  };
+        key: process.env.WEATHER_API_KEY,
+        city: req.query.search_query
+    };
 
-  let url = 'http://api.weatherbit.io/v2.0/forecast/daily';
-  return superagent.get(url).query(query).then(weatherData => {
-    try {
-      let arrayObject = [];
+    let url = 'http://api.weatherbit.io/v2.0/forecast/daily';
+    return superagent.get(url).query(query).then(weatherData => {
+        try {
+            let arrayObject = [];
 
-      weatherData.body.data.map(value => {
-        let time = value.datetime;
-        let x = turndate(time);
-        let forecast = value.weather.description;
-        let weatherObject = new CityWeather(forecast, x);
-        arrayObject.push(weatherObject);
-      });
-      return arrayObject;
-    } catch (error) {
-      res.status(500).send('Sorry, an error occured ..' + error);
-    }
-  }).catch(error => {
-    res.status(500).send('There was an error getting data from API ' + error);
-  });
+            weatherData.body.data.map(value => {
+                let time = value.datetime;
+                let x = turndate(time);
+                let forecast = value.weather.description;
+                let weatherObject = new CityWeather(forecast, x);
+                arrayObject.push(weatherObject);
+            });
+            return arrayObject;
+        } catch (error) {
+            res.status(500).send('Sorry, an error occured ..' + error);
+        }
+    }).catch(error => {
+        res.status(500).send('There was an error getting data from API ' + error);
+    });
 }
 
 
 //=========================turn date=============
 
 function turndate(day) {
-  let time = new Date(day);
-  const event = time;
-  const option = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
-  let chrTime = event.toLocaleDateString('en-US', option);
-  return chrTime;
+    let time = new Date(day);
+    const event = time;
+    const option = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+    let chrTime = event.toLocaleDateString('en-US', option);
+    return chrTime;
 }
 
 //====================Get location Data===============================
 
 function getLocationData(searchQuery) {
-  const query = {
-    key: process.env.GEOCODE_API_KEY,
-    q: searchQuery,
-    limit: 1,
-    format: 'json'
-  };
-  let url = 'https://us1.locationiq.com/v1/search.php';
-  return superagent.get(url).query(query).then(locationData => {
-    try {
-      let longatude = locationData.body[0].lon;
-      let latatude = locationData.body[0].lat;
-      let displayName = locationData.body[0].display_name;
-      let responseObject = new CityLocation(searchQuery, displayName, latatude, longatude);
-      return responseObject;
-    }
-    catch (error) {
-      res.status(500).send('Sorry, an error occured ..' + error);
-    }
-  }).catch(error => {
-    res.status(500).send('There was an error getting data from API ' + error);
-  });
+    const query = {
+        key: process.env.GEOCODE_API_KEY,
+        q: searchQuery,
+        limit: 1,
+        format: 'json'
+    };
+    let url = 'https://us1.locationiq.com/v1/search.php';
+    return superagent.get(url).query(query).then(locationData => {
+        try {
+            let longatude = locationData.body[0].lon;
+            let latatude = locationData.body[0].lat;
+            let displayName = locationData.body[0].display_name;
+            let responseObject = new CityLocation(searchQuery, displayName, latatude, longatude);
+            return responseObject;
+        }
+        catch (error) {
+            res.status(500).send('Sorry, an error occured ..' + error);
+        }
+    }).catch(error => {
+        res.status(500).send('There was an error getting data from API ' + error);
+    });
 
 }
 
@@ -130,20 +130,23 @@ function getLocationData(searchQuery) {
 // }
 
 function CityLocation(searchQuery, displayName, lot, lon) {
-  this.search_query = searchQuery;
-  this.formatted_query = displayName;
-  this.latitude = lon;
-  this.longitude = lot;
+    this.search_query = searchQuery;
+    this.formatted_query = displayName;
+    this.latitude = lon;
+    this.longitude = lot;
 }
 
 function CityWeather(forecast, time) {
-  this.forecast = forecast;
-  this.time = time;
+    this.forecast = forecast;
+    this.time = time;
 }
 
 //===================================================Parks=========================
 
-
-app.listen(PORT, () => {
-  console.log('the app is listen on port' + PORT);
+client.connect().then(() => {
+    app.listen(PORT, () => {
+        console.log('the app is listening to port ' + PORT);
+    });
+}).catch(error => {
+    console.log('an error occurred while connecting to database ' + error);
 });
