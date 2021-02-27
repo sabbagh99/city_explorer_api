502fa1a2d160e99a91b6c4f7ab3fd2fc143e2bcd
AHMAD ALSABBAGH <a.sameer1999@gmail.com>
2021-02-27 13:11:08 +0200

    check heroku

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const pg = require('pg');
 let app = express();
 app.use(cors());
 require('dotenv').config();
-const client = new pg.Client(process.env.DATABASE_URL);
-// const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
+// const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
 
 
 const PORT = process.env.PORT;
